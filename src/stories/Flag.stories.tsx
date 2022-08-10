05bd27336bcb34593ddce0902b34dbdcbf30f3db
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-10 14:34:22 +0200

    Add flag story

---
+++ src/stories/Flag.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Flag } from '../Flag';
+
+export default {
+  title: 'Flag',
+  component: Flag,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Flag>;
+
+const Template: ComponentStory<typeof Flag> = (args) => <Flag {...args} />;
+
+export const NormalFlag = Template.bind({});
+NormalFlag.args = {
+  countryCode: 'gb',
+};
+
+export const NormalFlagWithCustomPlaceholder = Template.bind({});
+NormalFlagWithCustomPlaceholder.args = {
+  countryCode: 'gb',
+  placeholder: 'Hello this is placeholder',
+};
